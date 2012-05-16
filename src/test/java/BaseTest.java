bb10f6d15553cde4cdd084e796c317e6c05cba69
Iain Rose <iain.rose@gmail.com>
2012-05-16 11:46:03 -0700

    - split up suiteSetup method in BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,12 +3,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
@@ -17,29 +19,24 @@ public class BaseTest {
     protected static final String WEB_SERVER = System.getProperty("WEB_SERVER", "http://stackoverflow.com/");
     protected static final String BROWSER = System.getProperty("BROWSER", "firefox");
     protected static final boolean REMOTE_DRIVER = Boolean.valueOf(System.getProperty("REMOTE_DRIVER", "false"));
-    protected static final String SELENIUM_HOST = System.getProperty("SELENIUM_HOST", "10.0.1.20");
+    protected static final String SELENIUM_HOST = System.getProperty("SELENIUM_HOST", "localhost");
     protected static final int SELENIUM_PORT = Integer.valueOf(System.getProperty("SELENIUM_PORT", "4444"));
 
-    public static WebDriver driver;
+    public static RemoteWebDriver driver;
 
     @BeforeClass(alwaysRun = true)
-    public void suiteSetup() throws Exception {
+    public void suiteSetup() throws MalformedURLException {
         if (REMOTE_DRIVER) {
-            DesiredCapabilities capabilities;
-            if (BROWSER.equals("firefox")) {
-                capabilities = DesiredCapabilities.firefox();
-            } else if (BROWSER.equals("internetExplorer")) {
-                capabilities = DesiredCapabilities.internetExplorer();
-                capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-            } else if (BROWSER.equals("chrome")) {
-                capabilities = DesiredCapabilities.chrome();
-            } else {
-                throw new RuntimeException("Browser type unsupported");
-            }
-            driver = new RemoteWebDriver(
-                    new URL("http://" + SELENIUM_HOST + ":" + SELENIUM_PORT + "/wd/hub"),
-                    capabilities);
-        } else if (BROWSER.equals("firefox")) {
+            setupRemoteDriver();
+            driver.setFileDetector(new LocalFileDetector());
+        } else {
+            setupLocalDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    private void setupLocalDriver() {
+        if (BROWSER.equals("firefox")) {
             driver = new FirefoxDriver();
         } else if (BROWSER.equals("chrome")) {
             String path = "lib/chromedriver";
@@ -55,7 +52,23 @@ public class BaseTest {
         } else {
             throw new RuntimeException("Browser type unsupported");
         }
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    private void setupRemoteDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities;
+        if (BROWSER.equals("firefox")) {
+            capabilities = DesiredCapabilities.firefox();
+        } else if (BROWSER.equals("internetExplorer")) {
+            capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        } else if (BROWSER.equals("chrome")) {
+            capabilities = DesiredCapabilities.chrome();
+        } else {
+            throw new RuntimeException("Browser type unsupported");
+        }
+        driver = new RemoteWebDriver(
+                new URL("http://" + SELENIUM_HOST + ":" + SELENIUM_PORT + "/wd/hub"),
+                capabilities);
     }
 
     @BeforeMethod(alwaysRun = true)
